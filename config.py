d999fdd337ed761baf42be480eb8fef502864bd6
unknown <parthvasoya2000@gmail.com>
2022-08-31 10:24:42 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+Dataset_Path = dict(
+    #CULane = "/home/lion/Dataset/CULane/data/CULane",
+    Tusimple = "E://Abhishek//Lane_Detection//CULane//parth//SCNN//SCNN_Pytorch-master//dataset//train_set"
+)
